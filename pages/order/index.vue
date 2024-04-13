20f7c5834ee3f5bbe0b87d2d6ad7c27fc0886f35
MrShahrom <shahromeo778@gmail.com>
2024-04-13 13:31:26 +0500

    added page order

--- pages/order/index.vue
+++ pages/order/index.vue
@@ -1,32 +1,136 @@
 <template>
-    <SidebarLayout>
-  
-    </SidebarLayout>
+  <SidebarLayout>
+  </SidebarLayout>
 
-    <NavbarLayout>
-      
-    </NavbarLayout>
-  
-  
-  </template>
-  
-  <script>
-  import SidebarLayout from '~/layouts/sidebar.vue';
-  import NavbarLayout from '~/layouts/navbar.vue';
-  
-  export default {
-    layout: 'sidebarLayout',
-    layout: 'NavbarLayout',
+  <NavbarLayout>
+  </NavbarLayout>
 
-    components: {
-      SidebarLayout,
-      NavbarLayout,
+  <section>
+    <div class="container2">
+      <h2>Таблица заказов</h2>
+      <div class="card-body order-table">
+        <NuxtLink to="/create" class="btn btn-primary btn-add">Добавить продукт</NuxtLink>
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>ФИО</th>
+              <th>Продукт</th>
+              <th>Дата заказа</th>
+              <th>Ед. из-я (сомони)</th>
+              <th>Цена</th>
+              <th>Общ. сумма</th>
+              <th>Количество</th>
+              <th>Тип заказа</th>
+              <th>Действие из.</th>
+              <th>Действие уд.</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="project in projects" :key="project.id">
+              <td>{{ project.id }}</td>
+              <td>{{ `${project.id_client.lastname} ${project.id_client.firstname} ${project.id_client.patronymic}` }}
+              </td>
+              <td>{{ project.id_product.name }}</td>
+              <td>{{ project.date_of_shipment }}</td>
+              <td>{{ project.units_of_measurement }}</td>
+              <td>{{ project.price_per_unit }}</td>
+              <td>{{ project.total_amount }}</td>
+              <td>{{ project.quantity }}</td>
+              <td>{{ project.type_of_sale }}</td>
+              <td>
+                <NuxtLink :to="`/edit/${project.id}`" class="btn btn-outline-success mx-1">Изменить</NuxtLink>
+              </td>
+              <td>
+                <button @click="handleDelete(project.id)" className="btn btn-danger mx-1">Удалить</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </section>
+
+
+
+</template>
+
+<script>
+import SidebarLayout from '~/layouts/sidebar.vue'
+import NavbarLayout from '~/layouts/navbar.vue'
+import { getProjects } from '~/services/projectService'
+
+
+export default {
+  layout: 'sidebarLayout',
+  layout: 'navbarLayout',
+  components: {
+    SidebarLayout,
+    NavbarLayout,
+  },
+  data() {
+    return {
+      projects: []
+    };
+  },
+  created() {
+    if (process.client) {
+      this.fetchProjectList();
+    }
+  },
+
+  methods: {
+    fetchProjectList() {
+      if (process.client) {
+        const token = localStorage.getItem('token');
+        if (!token) {
+          console.error('Token not found in local storage');
+          return;
+        }
+
+        const headers = {
+          'Authorization': `Bearer ${token}`
+        };
+
+        getProjects(headers)
+          .then(response => {
+            this.projects = response.data["data"];
+            console.log(response.data["data"])
+          })
+          .catch(error => {
+            if (error.response) {
+              console.error('Status code:', error.response.status);
+              console.error('Response data:', error.response.data);
+              console.error('Response headers:', error.response.headers);
+            } else if (error.request) {
+              console.error('No response received:', error.request);
+            } else {
+              console.error('Error setting up request:', error.message);
+            }
+          });
+      } else {
+        console.error('Trying to access localStorage on the server side.');
+      }
     }
   }
-  
-  </script>
-  
-  
-  <style>
-  
-  </style>
+
+
+};
+</script>
+
+
+
+
+<style scoped>
+.container2 {
+  padding-left: 150px;
+  padding-top: 60px;
+}
+
+h2 {
+  text-align: center;
+}
+.btn-add {
+  margin-bottom: 10px;
+}
+</style>
--- pages/product/index.vue
+++ pages/product/index.vue
@@ -1,75 +1,136 @@
 <template>
+  <SidebarLayout>
+  </SidebarLayout>
 
-  <h1>product</h1>
+  <NavbarLayout>
+  </NavbarLayout>
+
+  <section>
+    <div class="container2">
+      <h2>Таблица заказов</h2>
+      <div class="card-body order-table">
+        <NuxtLink to="/create" class="btn btn-primary btn-add">Добавить продукт</NuxtLink>
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>ФИО</th>
+              <th>Продукт</th>
+              <th>Дата заказа</th>
+              <th>Ед. из-я (сомони)</th>
+              <th>Цена</th>
+              <th>Общ. сумма</th>
+              <th>Количество</th>
+              <th>Тип заказа</th>
+              <th>Действие из.</th>
+              <th>Действие уд.</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="project in projects" :key="project.id">
+              <td>{{ project.id }}</td>
+              <td>{{ `${project.id_client.lastname} ${project.id_client.firstname} ${project.id_client.patronymic}` }}
+              </td>
+              <td>{{ project.id_product.name }}</td>
+              <td>{{ project.date_of_shipment }}</td>
+              <td>{{ project.units_of_measurement }}</td>
+              <td>{{ project.price_per_unit }}</td>
+              <td>{{ project.total_amount }}</td>
+              <td>{{ project.quantity }}</td>
+              <td>{{ project.type_of_sale }}</td>
+              <td>
+                <NuxtLink :to="`/edit/${project.id}`" class="btn btn-outline-success mx-1">Изменить</NuxtLink>
+              </td>
+              <td>
+                <button @click="handleDelete(project.id)" className="btn btn-danger mx-1">Удалить</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </section>
+
+
 
 </template>
 
 <script>
+import SidebarLayout from '~/layouts/sidebar.vue'
+import NavbarLayout from '~/layouts/navbar.vue'
+import { getProjects } from '~/services/projectService'
 
-import { deleteProject, getProjects } from '~/services/projectService'
-import Swal from 'sweetalert2'
 
 export default {
-
+  layout: 'sidebarLayout',
+  layout: 'navbarLayout',
+  components: {
+    SidebarLayout,
+    NavbarLayout,
+  },
   data() {
     return {
       projects: []
     };
   },
-
   created() {
-    this.fetchProjectList()
+    if (process.client) {
+      this.fetchProjectList();
+    }
   },
 
   methods: {
     fetchProjectList() {
-      getProjects()
-        .then(response => {
-          this.projects = response.data;
-          return response
-        })
-        .catch(error => {
-          return error
-        });
-
-    },
-
-    handleDelete(id) {
-      Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          deleteProject(id)
-            .then(response => {
-              Swal.fire({
-                icon: 'success',
-                title: 'Project deleted successfully!',
-                showConfirmButton: false,
-                timer: 1500
-              })
-              this.fetchProjectList();
-              return response
-            })
-            .catch(error => {
-              Swal.fire({
-                icon: 'error',
-                title: 'An Error Occured!',
-                showConfirmButton: false,
-                timer: 1500
-              })
-              return error
-            });
+      if (process.client) {
+        const token = localStorage.getItem('token');
+        if (!token) {
+          console.error('Token not found in local storage');
+          return;
         }
-      })
+
+        const headers = {
+          'Authorization': `Bearer ${token}`
+        };
+
+        getProjects(headers)
+          .then(response => {
+            this.projects = response.data["data"];
+            console.log(response.data["data"])
+          })
+          .catch(error => {
+            if (error.response) {
+              console.error('Status code:', error.response.status);
+              console.error('Response data:', error.response.data);
+              console.error('Response headers:', error.response.headers);
+            } else if (error.request) {
+              console.error('No response received:', error.request);
+            } else {
+              console.error('Error setting up request:', error.message);
+            }
+          });
+      } else {
+        console.error('Trying to access localStorage on the server side.');
+      }
     }
-
-
   }
+
+
 };
 </script>
+
+
+
+
+<style scoped>
+.container2 {
+  padding-left: 150px;
+  padding-top: 60px;
+}
+
+h2 {
+  text-align: center;
+}
+.btn-add {
+  margin-bottom: 10px;
+}
+</style>
